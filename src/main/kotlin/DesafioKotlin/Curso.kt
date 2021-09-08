888912383b5b4e9119511986eab44732b28b97bd
Denise Azevedo <denise.azevedopereira@gmail.com>
2021-09-07 21:36:26 -0300

    Parte C

--- src/main/kotlin/DesafioKotlin/Curso.kt
+++ src/main/kotlin/DesafioKotlin/Curso.kt
@@ -1,5 +1,5 @@
 package DesafioKotlin
 
-class Curso(var nome: String, codigoCurso: Int) {
-    // TODO: 07/09/2021 Parte B 
+class Curso(var nome: String, var codigoCurso: Int) {
+    // TODO: 07/09/2021 Parte B
 }
---
+++ src/main/kotlin/DesafioKotlin/Professor.kt
@@ -0,0 +1,6 @@
+package DesafioKotlin
+
+class Professor(var nome: String, var sobrenome: String, var tempoDeCasa: Int, var codigoProfessor: Int) {
+    // TODO: 07/09/2021 Parte C
+
+}
