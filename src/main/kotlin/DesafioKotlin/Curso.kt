128c5b8a5d9d1c7ef1aa5ab42a09e2d33164d289
Denise Azevedo <denise.azevedopereira@gmail.com>
2021-09-09 09:49:49 -0300

    Parte E

--- src/main/kotlin/DesafioKotlin/Curso.kt
+++ src/main/kotlin/DesafioKotlin/Curso.kt
@@ -1,5 +1,12 @@
 package DesafioKotlin
 
-class Curso(var nome: String, var codigoCurso: Int) {
+class Curso(
+    var nome: String,
+    var codigoCurso: Int,
+    var profTitular: ProfessorTitular,
+    var profAdjunto: ProfessorAdjunto,
+    var quantidadeMaxAlunos: Int,
+    var listaAlunosMatriculados: MutableList<Aluno>
+) {
     // TODO: 07/09/2021 Parte B
 }
